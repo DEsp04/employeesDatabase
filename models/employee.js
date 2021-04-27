a1e519ab3222f1751b3ddf20ea092c1b35952d89
david espinal <despinal0425@gmail.com>
2021-04-27 11:44:49 -0400

    created the child schema

---
+++ models/employee.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const Employee = new Schema(
+  {
+    _id: { type: Schema.Types.ObjectId, ref: "employee_id" },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
+    job_title: { type: String, required: true },
+    address: {
+      stree: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      zip: { type: String, required: true },
+    }
+    
+  }
+);
+
+module.exports = mogoose.model('employee', Employee);
